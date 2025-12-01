c4de657c028e2d950f778de3f1bdd232b0ad2acf
Công Hiền <147261809+CongHien05@users.noreply.github.com>
2025-12-01 17:31:43 +0700

    OK

--- BankingServer/src/common/IClientCallback.java
+++
@@ -1,9 +0,0 @@
-package common;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface IClientCallback extends Remote {
-    // Phương thức Server sẽ gọi để gửi thông báo cho Client
-    void notify(String message) throws RemoteException;
-}
